c89be2d772bd48cc4fd51cdd3daf15ec79fc1c8f
Leifur16 <leifur16@ru.is>
2018-10-11 16:59:50 +0000

    working on getAuctionBids

--- template/schemas/auction.js
+++ template/schemas/auction.js
@@ -1,8 +1,8 @@
 const Schema = require("mongoose").Schema;
 
 module.exports = new Schema({
-  artId: { type: Schema.Types.ObjectId, required: true, ref: "art" },
+  artId: { type: Schema.Types.ObjectId, required: true, ref: "Art" },
   minimumPrice: { type: Number, required: true, default: 1000 },
   endDate: { type: Date, required: true },
-  auctionWinner: { type: Schema.Types.ObjectId }
+  auctionWinner: { type: Schema.Types.ObjectId, ref: "Customer" }
 });
--- template/schemas/auctionBid.js
+++ template/schemas/auctionBid.js
@@ -1,7 +1,7 @@
 const Schema = require("mongoose").Schema;
 
 module.exports = new Schema({
-  auctionId: { type: Schema.Types.ObjectId, required: true },
-  customerId: { type: Schema.Types.ObjectId, required: true },
+  auctionId: { type: Schema.Types.ObjectId, required: true, ref: "Auction" },
+  customerId: { type: Schema.Types.ObjectId, required: true, ref: "Customer" },
   price: { type: Number, required: true }
 });
